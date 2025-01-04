6ae0f89330eb2e7157cb1999568104ae87ad2226
alexundros <alex-vlz@mail.ru>
2025-01-04 19:48:01 +0300

    add start sonar scanner scripts

---
+++ tools/start-sonar-scanner.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "%~dp0.."
+docker run --rm -v "%cd%:/usr/src" ^
+ -e SONAR_HOST_URL=%SONAR_HOST_URL:http://host.docker.internal:10000% ^
+ --name sonar-scanner-maven_check_versions sonarsource/sonar-scanner-cli ^
+ -Dsonar.login=%SONAR_LOGIN:admin% ^
+ -Dsonar.password=%SONAR_PASSWORD:sonar% ^
+ -Dsonar.sourceEncoding=UTF-8 ^
+ -Dsonar.projectBaseDir=. -Dsonar.inclusions=**/*.py ^
+ -Dsonar.projectKey=maven_check_versions ^
+ -Dsonar.projectName="Maven Check Versions"
