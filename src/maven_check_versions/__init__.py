2f0cd27a69cb3d96a9daea4fe41adb977af0f0f5
alexundros <alex-vlz@mail.ru>
2025-02-07 09:33:49 +0300

    added default none for 'store_true' cli arguments

--- src/maven_check_versions/__init__.py
+++ src/maven_check_versions/__init__.py
@@ -34,21 +34,22 @@ def parse_command_line_arguments() -> dict:
     argument_parser.add_argument('-pf', '--pom_file', help='Path to POM File')
     argument_parser.add_argument('-fa', '--find_artifact', help='Artifact to find')
     # override for config file options
-    argument_parser.add_argument('-co', '--cache_off', help='Disable Cache', action='store_true')
+    argument_parser.add_argument('-co', '--cache_off', help='Disable Cache', action='store_true', default=None)
     argument_parser.add_argument('-cf', '--cache_file', help='Path to Cache File')
     argument_parser.add_argument('-ct', '--cache_time', help='Cache expiration time in seconds')
-    argument_parser.add_argument('-lfo', '--logfile_off', help='Disable Log file', action='store_true')
+    argument_parser.add_argument('-lfo', '--logfile_off', help='Disable Log file', action='store_true', default=None)
     argument_parser.add_argument('-lf', '--log_file', help='Path to Log File')
     argument_parser.add_argument('-cfg', '--config_file', help='Path to Config File')
-    argument_parser.add_argument('-fm', '--fail_mode', help='Enable Fail Mode', action='store_true')
+    argument_parser.add_argument('-fm', '--fail_mode', help='Enable Fail Mode', action='store_true', default=None)
     argument_parser.add_argument('-mjv', '--fail_major', help='Major version threshold for failure')
     argument_parser.add_argument('-mnv', '--fail_minor', help='Minor version threshold for failure')
-    argument_parser.add_argument('-sp', '--search_plugins', help='Search plugins', action='store_true')
-    argument_parser.add_argument('-sm', '--process_modules', help='Process modules', action='store_true')
-    argument_parser.add_argument('-sk', '--show_skip', help='Show Skip', action='store_true')
-    argument_parser.add_argument('-ss', '--show_search', help='Show Search', action='store_true')
-    argument_parser.add_argument('-ev', '--empty_version', help='Allow empty version', action='store_true')
-    argument_parser.add_argument('-si', '--show_invalid', help='Show Invalid', action='store_true')
+    argument_parser.add_argument('-sp', '--search_plugins', help='Search plugins', action='store_true', default=None)
+    argument_parser.add_argument('-sm', '--process_modules', help='Process modules', action='store_true', default=None)
+    argument_parser.add_argument('-sk', '--show_skip', help='Show Skip', action='store_true', default=None)
+    argument_parser.add_argument('-ss', '--show_search', help='Show Search', action='store_true', default=None)
+    argument_parser.add_argument(
+        '-ev', '--empty_version', help='Allow empty version', action='store_true', default=None)
+    argument_parser.add_argument('-si', '--show_invalid', help='Show Invalid', action='store_true', default=None)
     argument_parser.add_argument('-un', '--user', help='Basic Auth user')
     argument_parser.add_argument('-up', '--password', help='Basic Auth password')
     return vars(argument_parser.parse_args())
@@ -756,11 +757,11 @@ def get_config_value(
         if value is None:
             value = config_parser.get(section, key)
         if value_type == bool:
-            return str(value).lower() == 'true'
+            value = str(value).lower() == 'true'
         if value_type == int:
-            return int(value)
+            value = int(value)
         if value_type == float:
-            return float(value)
+            value = float(value)
         return value
     except configparser.Error:
         return None
