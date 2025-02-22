263c23cea55f04e1d04483b4aa5b178fea337a9e
alexundros <alex-vlz@mail.ru>
2025-02-23 00:49:16 +0300

    updated workflows `python-package-publish-to-pypi.yml`

--- tools/dist/upload-dist/twine-upload-dist.sh
+++ tools/dist/upload-dist/twine-upload-dist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/../.." || exit 1
-[ -z "$PYPY_TOKEN" ] && echo "PYPY_TOKEN ERROR" && exit 1
-python -m twine upload dist/* --username __token__ --password "$PYPY_TOKEN"
+[ -z "$PYPI_TOKEN" ] && echo "PYPI_TOKEN ERROR" && exit 1
+python -m twine upload dist/* -u __token__ -p "$PYPI_TOKEN"
